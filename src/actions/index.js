911744198332a6a7ad29ed568ae686f3a08f45f4
Dan Constantinescu <dan.constantinescu@softvision.ro>
2017-10-03 09:25:51 +0300

    created the store

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,9 @@
 import {ADD_REMINDER} from '../constants/constants';
+export const addReminder = (text) => {
+    const action = {
+        type: ADD_REMINDER,
+        text
+    }
+    console.log('action in addReminder', action);
+    return action;
+}
